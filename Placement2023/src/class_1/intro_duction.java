d3bdb7a3df09da6ece59d281f8999a7e91250838
Sarthak Sharma <contactsarthak2014@gmail.com>
2022-07-17 00:22:29 +0530

    first commit

---
+++ Placement2023/src/class_1/intro_duction.java
@@ -0,0 +1,15 @@
+package class_1;
+
+import java.util.Scanner;
+
+public class intro_duction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		System.out.println(a+b);
+	}
+
+}
